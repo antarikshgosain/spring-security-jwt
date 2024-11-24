2d3b61121d0acf9fade96380279e26f14416ffc8
antarikshgosain <antariksh.gosain@gmail.com>
2024-11-24 15:02:28 -0500

    creating an InMemory User and printing username in response

--- jwt-demo/src/main/java/com/secureapp/jwtdemo/config/SecurityConfig.java
+++ jwt-demo/src/main/java/com/secureapp/jwtdemo/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.*;
@@ -13,6 +15,16 @@ import static org.springframework.security.config.Customizer.*;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("anta")
+                        .password("{noop}anta01")
+                        .authorities("read")
+                        .build()
+        );
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- jwt-demo/src/main/java/com/secureapp/jwtdemo/controller/HomeController.java
+++ jwt-demo/src/main/java/com/secureapp/jwtdemo/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.secureapp.jwtdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/home")
-    public String home(){
-        return "Hello JWT";
+    public String home(Principal principal){
+        return "Hello " + principal.getName();
     }
 
 }
