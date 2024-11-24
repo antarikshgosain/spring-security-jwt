c7f28b14437c13188df2b924b7a7d2e5a4f7c18f
antarikshgosain <antariksh.gosain@gmail.com>
2024-11-24 17:28:09 -0500

    Beans for JWT encoder & decoder created

--- jwt-demo/src/main/java/com/secureapp/jwtdemo/JwtDemoApplication.java
+++ jwt-demo/src/main/java/com/secureapp/jwtdemo/JwtDemoApplication.java
@@ -1,9 +1,12 @@
 package com.secureapp.jwtdemo;
 
+import com.secureapp.jwtdemo.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwtDemoApplication {
 
 	public static void main(String[] args) {
---
+++ jwt-demo/src/main/java/com/secureapp/jwtdemo/TokenService.java
@@ -0,0 +1,44 @@
+package com.secureapp.jwtdemo;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+
+@Service // Hey Springframework, please manage this class by sticking it in Application context, so irt can be used
+public class TokenService {
+
+    private final JwtEncoder jwtEncoder;
+
+
+    public TokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(30, ChronoUnit.MINUTES))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        String tokenValue = this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        return tokenValue;
+    }
+
+
+
+}
